6f9b0964b0864b3b36d0a7395b32d425546911d9
Louise Lalu <louiserafaellalu@gmail.com>
2023-11-08 15:22:50 +0800

    add caching in visualizer to minimize cpu usage

---
+++ visualizer_v2.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from PIL import Image, ImageDraw, ImageTk
+import json
+
+filename = 'resume_1_1'
+
+annotations_dir = './annotations_louise'
+images_dir = './annotations_images_louise'
+
+with open(f'{annotations_dir}/{filename}.json', 'r', encoding='utf-8') as file:
+    cache = json.load(file)
+
+first_run = True
+
+def update_image(cache: dict):
+    global img, draw, canvas, image_item, first_run
+
+    # Reload the JSON data
+    with open(f'{annotations_dir}/{filename}.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    if data != cache or first_run:
+        if first_run:
+            first_run = False
+
+        cache = data
+
+        # Load the image
+        img = Image.open(f'{images_dir}/{filename}.jpeg')
+        draw = ImageDraw.Draw(img)
+
+        for entry in data['form']:
+            # The following code visualizes the bounding boxes of a single entry
+            box = entry['box']
+            label = entry['label']  # Get the label name
+            draw.rectangle(box, outline='red', width=2)
+            if label:
+                # Display the label name near the bounding box
+                draw.text((box[0], box[1] - 10), label, fill='red')
+
+        # Update the canvas with the new image and adjust the window size
+        width, height = img.size
+        canvas.config(width=width, height=height)
+        photo = ImageTk.PhotoImage(img)
+        canvas.itemconfig(image_item, image=photo)
+        canvas.image = photo
+
+    # Schedule the next update in 1 second (adjust as needed)
+    root.after(1000, update_image, cache)
+
+# Create the main application window
+root = tk.Tk()
+root.title("Real-time Bounding Box Viewer")
+
+# Create a Canvas to display the image
+canvas = tk.Canvas(root)
+canvas.pack()
+
+# Load and display the image initially
+img = Image.open(f'{images_dir}/{filename}.jpeg')
+photo = ImageTk.PhotoImage(img)
+image_item = canvas.create_image(0, 0, anchor=tk.NW, image=photo)
+
+# Update the image
+update_image(cache)
+
+# Start the main loop
+root.mainloop()
